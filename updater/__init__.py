10982fb3dc3e6d9bf7f497db8cca39e4e31c6cec
Alexandre Hervé <alexandreherve@protonmail.com>
2021-07-20 15:42:15 +0200

    Setup updater

--- updater/__init__.py
+++ updater/__init__.py
@@ -1,5 +1,15 @@
-from subprocess import check_call
+from subprocess import run
+import os
+
+script = """
+DIR="/tmp/defense-finder"
+rm -rf $DIR
+mkdir $DIR
+macsydata download --org mdmparis -d $DIR defense-finder-models 
+ARCHIVE=$(ls $DIR | head -n 1)
+macsydata install --org mdmparis "$DIR/$ARCHIVE"
+rm -rf $DIR
+"""
 
 def update_models():
-    bashCommand = 'touch test.txt'
-    check_call(bashCommand.split())
+    run(script, shell=True)
