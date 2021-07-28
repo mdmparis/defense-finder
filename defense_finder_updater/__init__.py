d9b600b9798aac1f59b4a1363eb228b813df581e
Alexandre Hervé <alexandreherve@protonmail.com>
2021-07-28 10:42:17 +0200

    Fix update script

--- defense_finder_updater/__init__.py
+++ defense_finder_updater/__init__.py
@@ -1,13 +1,7 @@
 from subprocess import run
 
 script = """
-DIR="/tmp/defense-finder-models"
-rm -rf $DIR
-mkdir $DIR
-macsydata download --org mdmparis -d $DIR defense-finder-models 
-ARCHIVE=$(ls $DIR | head -n 1)
-macsydata install --org mdmparis "$DIR/$ARCHIVE"
-rm -rf $DIR
+macsydata install -U -u --org mdmparis defense-finder-models
 """
 
 def update_models():
