473fc263b44f07359f6a1839b89cfe1910c8d4e9
Aurélien Hervé <mail@aurelien-herve.com>
2022-02-08 17:11:38 +0100

    allow defense_finder to run on files containing spaces

--- defense_finder/__init__.py
+++ defense_finder/__init__.py
@@ -9,6 +9,21 @@ macsyfinder --db-type {dbtype} --sequence-db {file} --models defense-finder-mode
 macsyfinder --db-type {dbtype} --sequence-db {file} --models defense-finder-models/DefenseFinder_5 all --out-dir {dir}/DF_5 --coverage-profile 0.1  --w {workers} --exchangeable-weight 1
 macsyfinder --db-type {dbtype} --sequence-db {file} --models defense-finder-models/RM all --out-dir {dir}/RM --coverage-profile 0.1  --w {workers} --exchangeable-weight 1
 macsyfinder --db-type {dbtype} --sequence-db {file} --models defense-finder-models/Cas all --out-dir {dir}/Cas --accessory-weight 1 --exchangeable-weight 1 --coverage-profile 0.4 -w {workers}
-""".format(workers=workers, file=f.name, dbtype=dbtype, dir=tmp_dir)
+""".format(workers=workers, file=escapeFileName(f.name), dbtype=dbtype, dir=tmp_dir)
 
     subprocess.run(script, shell=True)
+
+def escapeFileName(f):
+    r = ""
+    for char in f:
+        if char >= "a" and char <= "z":
+            r += char
+        elif char >= "A" and char <= "Z":
+            r += char
+        elif char >= "0" and char <= "9":
+            r += char
+        elif char in ["-", "_", ".", "/"]:
+            r += char
+        else:
+            r += "\\" + char
+    return r
