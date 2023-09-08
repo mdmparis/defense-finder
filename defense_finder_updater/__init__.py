c15e0356502594ce99591cf9f87e0f052dd2e256
jeanrjc <jean.cury@normalesup.org>
2023-09-08 14:51:37 +0200

    Install models in user's home by default

--- defense_finder_updater/__init__.py
+++ defense_finder_updater/__init__.py
@@ -3,12 +3,12 @@ from macsypy.scripts import macsydata
 
 def update_models(models_dir):
     # Updating DefenseFinder models
-    args_models_dir = f"-t {models_dir}" if models_dir is not None else ""
+    args_models_dir = f"-t {models_dir}" if models_dir is not None else "-u"
     cmd_args = f"install -U {args_models_dir} --org mdmparis defense-finder-models"
     macsydata.main(shlex.split(cmd_args))
 
     # Updating CASFinder models
-    args_models_dir = f"-t {models_dir}" if models_dir is not None else ""
+    args_models_dir = f"-t {models_dir}" if models_dir is not None else "-u"
     cmd_args = f"install -U {args_models_dir}  CasFinder"
     macsydata.main(shlex.split(cmd_args))
 
