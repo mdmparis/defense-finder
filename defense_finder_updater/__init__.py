4a5da7550df3108f80da8d9e4a0d63faba62f4c4
jeanrjc <jean.cury@normalesup.org>
2023-09-08 14:59:38 +0200

    change case for CasFinder

--- defense_finder_updater/__init__.py
+++ defense_finder_updater/__init__.py
@@ -9,6 +9,6 @@ def update_models(models_dir):
 
     # Updating CASFinder models
     args_models_dir = f"-t {models_dir}" if models_dir is not None else "-u"
-    cmd_args = f"install -U {args_models_dir}  CasFinder"
+    cmd_args = f"install -U {args_models_dir} CasFinder"
     macsydata.main(shlex.split(cmd_args))
 
