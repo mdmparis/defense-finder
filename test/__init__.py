455d8783f95d0067a153a0f28b281e04583a22a7
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2023-10-19 14:20:05 +0200

    add context to catch stdout et stderr

--- test/__init__.py
+++ test/__init__.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import unittest
+from contextlib import contextmanager
+from io import StringIO
 
 
 class FakeExitCodeException(Exception):
@@ -23,3 +25,22 @@ class TooledTest(unittest.TestCase):
     def fake_exit(*args, **kwargs):
         returncode = args[0]
         raise FakeExitCodeException(returncode)
+
+    @contextmanager
+    def catch_io(self, out=False, err=False):
+        """
+        Catch stderr and stdout of the code running within this block.
+        """
+        old_out = sys.stdout
+        new_out = old_out
+        old_err = sys.stderr
+        new_err = old_err
+        if out:
+            new_out = StringIO()
+        if err:
+            new_err = StringIO()
+        try:
+            sys.stdout, sys.stderr = new_out, new_err
+            yield sys.stdout, sys.stderr
+        finally:
+            sys.stdout, sys.stderr = old_out, old_err
