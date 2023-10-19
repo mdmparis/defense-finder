9243b5aee5ace9d92302ff7556e7698f994d8acd
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2023-10-19 14:29:03 +0200

    test all files in test data dir

--- test/test_functional_test.py
+++ test/test_functional_test.py
@@ -4,26 +4,51 @@ from defense_finder_cli.main import run
 from test import TooledTest, FakeExitCodeException
 
 
-class Test(TooledTest):
-    def test_data_content(self):
-        subdir = "tests/data/nt/"
-        input_file = "df_test_nt.fna"
-        expected_results_dir = "expected_results/"
+def cleaned_lines(expected):
+    return set(sorted([line.upper() for line in expected.readlines()]))
 
-        with self.assertRaises(FakeExitCodeException) as ctx:
-            run(
-                [
-                    subdir + input_file,
-                    "-o",
-                    subdir,
-                ]
-            )
+
+class Test(TooledTest):
+    data_dir = "test/data"
+    res_dir = "expected_results"
+
+    def test_data_content(self):
+        data_dirs = []
+        for subdir in os.listdir(self.data_dir):
+            # get all subdir of data
+            subdir = os.path.join(self.data_dir, subdir)
+            # but only if it contains expected_dir
+            if not os.path.exists(os.path.join(subdir, self.res_dir)):
+                continue
+            for input_file in os.listdir(subdir):
+                # all all fna and faa files, append them
+                if input_file.endswith(".fna") or input_file.endswith(".faa"):
+                    data_dirs.append((subdir, input_file))
+        # one subtest per file
+        for subdir, input_file in data_dirs:
+            with self.subTest(subdir=subdir):
+                self._test_subdir(subdir=subdir, input_file=input_file)
+
+    def _test_subdir(self, subdir, input_file):
+        with self.catch_io(out=True, err=True) as (out, err):
+            with self.assertRaises(FakeExitCodeException) as ctx:
+                run(
+                    [
+                        os.path.join(subdir, input_file),
+                        "-o",
+                        subdir,
+                    ]
+                )
         self.assertEqual(str(ctx.exception), "0")  # check the returncode
 
-        for file in os.listdir(os.path.join(subdir, expected_results_dir)):
-            with open(
-                os.path.join(subdir, expected_results_dir, file), "r"
-            ) as expected, open(os.path.join(subdir, file), "r") as produced:
-                expected_lines = set(expected.readlines())
-                produced_lines = set(produced.readlines())
-                self.assertSetEqual(expected_lines, produced_lines, file)
+        try:
+            for file in os.listdir(os.path.join(subdir, self.res_dir)):
+                with open(
+                    os.path.join(subdir, self.res_dir, file), "r"
+                ) as expected, open(os.path.join(subdir, file), "r") as produced:
+                    expected_lines = cleaned_lines(expected)
+                    produced_lines = cleaned_lines(produced)
+                    self.assertSetEqual(expected_lines, produced_lines, file)
+        except AssertionError as ae:
+            print(out.getvalue().strip(), err.getvalue().strip())
+            raise ae
