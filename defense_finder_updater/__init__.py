2a381bcbcefa2787675f4ac0ede287a922421b13
jeanrjc <jean.cury@normalesup.org>
2025-02-05 15:57:11 +0100

    silence macsyfinder warning about having gitpython

--- defense_finder_updater/__init__.py
+++ defense_finder_updater/__init__.py
@@ -1,5 +1,6 @@
 import shlex
-from macsypy.scripts import macsydata
+
+from macsypy.scripts.macsydata import main as mdmain
 
 
 def update_models(models_dir, force_reinstall: bool):
@@ -7,10 +8,10 @@ def update_models(models_dir, force_reinstall: bool):
     args_models_dir = f"-t {models_dir}" if models_dir is not None else "-u"
     args_force = "-f" if force_reinstall else ""
     cmd_args = f"install -U {args_models_dir} {args_force} --org mdmparis defense-finder-models"
-    macsydata.main(shlex.split(cmd_args))
+    mdmain(shlex.split(cmd_args))
 
     # Updating CASFinder models
     args_models_dir = f"-t {models_dir}" if models_dir is not None else "-u"
     cmd_args = f"install -U {args_models_dir} {args_force} CasFinder"
-    macsydata.main(shlex.split(cmd_args))
+    mdmain(shlex.split(cmd_args))
 
